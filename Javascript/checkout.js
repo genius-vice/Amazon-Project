4e8377113c375049e362cb401c870cb52e81c30d
genius-vice <am2383364@gmail.com>
2025-10-06 23:26:50 +0100

    delete html when clicking delete

--- Javascript/checkout.js
+++ Javascript/checkout.js
@@ -14,7 +14,8 @@ cart.forEach((cartItem) => {
         }
     });
     // console.log(matchingProduct);
-        cartSummary += `<div class="cart-item-container">
+        cartSummary += `<div class="cart-item-container 
+        js-cart-item-container-${matchingProduct.id}">
     <div class="delivery-date">
     Delivery date: Wednesday, June 15
     </div>
@@ -97,6 +98,9 @@ document.querySelectorAll('.js-delete-link')
     link.addEventListener('click', () => {
         const productId = link.dataset.productId;
         removeFromCart(productId);
-        console.log(cart)
+        const container = document.querySelector(`
+            .js-cart-item-container-${productId}`);
+            container.remove();
+
     });
 });
