ee27686c17bd34e31e3a7e7fb3680ecb60f3303c
genius-vice <am2383364@gmail.com>
2025-10-06 22:59:36 +0100

    Delete product from the cart when clicking delete

--- Javascript/checkout.js
+++ Javascript/checkout.js
@@ -1,4 +1,4 @@
-import {cart} from '../data/cart.js';
+import {cart, removeFromCart} from '../data/cart.js';
 import {products} from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 
@@ -13,7 +13,7 @@ cart.forEach((cartItem) => {
             matchingProduct = product;
         }
     });
-    console.log(matchingProduct);
+    // console.log(matchingProduct);
         cartSummary += `<div class="cart-item-container">
     <div class="delivery-date">
     Delivery date: Wednesday, June 15
@@ -37,7 +37,8 @@ cart.forEach((cartItem) => {
         <span class="update-quantity-link link-primary">
             Update
         </span>
-        <span class="delete-quantity-link link-primary">
+        <span class="delete-quantity-link link-primary js-delete-link"
+        data-product-id="${matchingProduct.id}">
             Delete
         </span>
         </div>
@@ -89,4 +90,13 @@ cart.forEach((cartItem) => {
 </div>`;
 });
 document.querySelector('.js-order-summary').innerHTML = cartSummary;
-console.log(cartSummary);
+
+
+document.querySelectorAll('.js-delete-link')
+.forEach((link) => {
+    link.addEventListener('click', () => {
+        const productId = link.dataset.productId;
+        removeFromCart(productId);
+        console.log(cart)
+    });
+});
